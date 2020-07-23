c98a6fa8fe56adcec6afbbe64b5fc8fe8a91f94b
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 22:35:57 -0700

    Update assignment1.asm

--- Assignments/Assignment 1/assignment1.asm
+++ Assignments/Assignment 1/assignment1.asm
@@ -1,7 +1,7 @@
 ;=========================================================================
 ; Name & Email must be EXACTLY as in Gradescope roster!
 ; Name: Lau, Jasmine
-; Email: jlau053@ucr.edu
+; Email: 
 ; 
 ; Assignment name: Assignment 1
 ; Lab section: A021
