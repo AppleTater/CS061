e500ae2edbf016be1bd7a49adac0e417b631639b
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 22:51:42 -0700

    Update assignment5.asm

--- Assignments/Assignment 5/assignment5.asm
+++ Assignments/Assignment 5/assignment5.asm
@@ -6,11 +6,6 @@
 ; Assignment name: Assignment 5
 ; Lab section: A021
 ; TA: Jason Goulding
-; 
-; I hereby certify that I have not received assistance on this assignment,
-; or used code, from ANY outside source other than the instruction team
-; (apart from what was provided in the starter file).
-;
 ;=========================================================================
 ;-------------
 ;Instructions
