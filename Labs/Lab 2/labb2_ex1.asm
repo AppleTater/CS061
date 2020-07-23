8e2bb8fa5c9c2bbf16cd908adb2c7290c3360512
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 22:32:54 -0700

    Create labb2_ex1.asm

---
+++ Labs/Lab 2/labb2_ex1.asm
@@ -0,0 +1,18 @@
+;=================================================
+; Name: Jasmine Lau
+; Email: jlau053@ucr.edu
+; 
+; Lab: lab 2. ex 1
+; Lab section: A21
+; TA: Jason Goulding
+; 
+;=================================================
+.orig x3000
+
+	LD R3, DEC_65	;R3 <-- #65
+	LD R4, HEX_41	;R4 <-- x41
+	
+	DEC_65	.FILL #65	;Fills a memory location with #65
+	HEX_41	.FILL x41	;Fills a memory location with x41
+	
+.END
