42262c22225b69cb9ae8b90f3b3343b44d6ae8c0
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-22 22:32:29 -0700

    Create lab1_ex1.asm

---
+++ Labs/Lab 1/lab1_ex1.asm
@@ -0,0 +1,28 @@
+;=================================================
+; Name: Lau, Jasmine
+; Email:  jlau053@ucr.edu
+; 
+; Lab: lab 1, ex 1
+; Lab section: A21
+; TA: Jason Goulding
+; 
+;=================================================
+.orig x3000
+
+	AND R1, R1, x0
+	LD R2, DEC_12
+	LD R3, DEC_6
+	
+	DO_WHILE_LOOP
+		ADD R1, R1, R2
+		ADD R3, R3, #-1
+		BRp DO_WHILE_LOOP
+	END_DO_WHILE_LOOP
+	
+	HALT
+	
+	DEC_0	.FILL 	#0
+	DEC_12	.FILL 	#12
+	DEC_6	.FILL 	#6
+	
+.END
