619e0576e586205b1e29492df92b976d32253d1a
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-07-23 09:45:20 -0700

    Update lab9_ex1 and 2.asm

--- Labs/Lab 9/lab9_ex1 and 2.asm
+++ Labs/Lab 9/lab9_ex1 and 2.asm
@@ -8,187 +8,197 @@
 ; 
 ;=================================================
 
-; test harness
 .orig x3000
-LD R4, base_ptr
-LD R5, max_ptr
-LD R6, base_ptr
+
+LD R2, NEWLINE
+
+NOT R2, R2
+ADD R2, R2, #1
   
-PUSH_LOOP
-  LEA R0,push_prompt
+LD R4, BASE
+LD R5, MAX
+LD R6, TOS
+
+WHILE_LOOP
+  LEA R0, PROMPT_1
   PUTS
-  
-  AND R0, R0, #0
+
   GETC
   OUT
 
-  LD R2, zero
-  ADD R0, R0, R2
+  LD R1, OFFSET
+  ADD R3, R0, R1
+  BRz POP_STACK
 
-  LD R1, SUB_STACK_PUSH_ADDY
-  JSRR  R1
-  
-  ADD R3, R3, #0
-  BRnz PUSH_LOOP
-  BRp POP_LOOP
-
-POP_LOOP
-  LEA R0, pop_prompt
+  LEA R0, PROMPT_2
   PUTS
-  
-  LD R1,SUB_STACK_POP_ADDY
+
+  GETC
+  OUT
+
+  ADD R3, R0, R2
+  BRz END_LOOP
+
+  ST R0, USER_INPUT
+  LD R0, NEWLINE
+
+  OUT
+  LD R0, USER_INPUT
+  LD R1, OFFSET
+
+  ADD R0, R0, R1
+  LD R1, SUB_STACK_PUSH
+
   JSRR R1
-  
-  ADD R3, R3, #0 
-  BRnz POP_LOOP
-  BRp END_POP
-  
-  END_POP
-	halt
+  BRnzp WHILE_LOOP
 
-;-----------------------------------------------------------------------------------------------
-; test harness local data:
-zero				.FILL #-48
+POP_STACK
+  LD R0, NEWLINE
+  OUT
 
-push_prompt			.STRINGZ "ENTER VALUE TO PUSH\n"
-pop_prompt			.STRINGZ "ENTER VALUE TO POP\n"
+  AND R0, R0, #0 
+  LD R1, SUB_STACK_POP
 
-base_ptr			.FILL x4000
-max_ptr				.FILL x4005
+  JSRR R1
+  LD R1, OFFSET
 
-SUB_STACK_PUSH_ADDY	.FILL x3200
-SUB_STACK_POP_ADDY 	.FILL x3400
-;===============================================================================================
-halt
+  NOT R1, R1
+  ADD R1, R1, #1
+  ADD R0, R0, R1
 
-; subroutines:
+  BRn WHILE_LOOP
+  NOT R1, R0
+  ADD R1, R1, #1
+
+  LD R2, NUM_MAX
+  ADD R2, R2, R1
+  BRn WHILE_LOOP
+  OUT
+
+  LEA R0, PROMPT_3
+  PUTS
+
+  BRnzp WHILE_LOOP
+
+END_LOOP
+  HALT
+
+;Local Data
+
+SUB_STACK_PUSH 	.FILL x3200
+SUB_STACK_POP 	.FILL x3400
+
+OFFSET   		.FILL #-48
+NUM_MAX  	 	.FILL x39
+NEWLINE  		.FILL x0A
+
+BASE 	 		.FILL xA000
+MAX 			.FILL xA005
+TOS 			.FILL xA000
+
+PROMPT_1 		.STRINGZ "Pop (0) or Push (1)\n"
+PROMPT_2 		.STRINGZ "\nWhat value would you like to push?\n"
+PROMPT_3 		.STRINGZ " was the value popped\n"
+
+USER_INPUT 		.BLKW 1
+
 
 ;------------------------------------------------------------------------------------------
-; Subroutine: SUB_STACK_PUSH
+;Subroutine: SUB_STACK_PUSH
 ; Parameter (R0): The value to push onto the stack
 ; Parameter (R4): BASE: A pointer to the base (one less than the lowest available
-;                       address) of the stack
+; address) of the stack
 ; Parameter (R5): MAX: The "highest" available address in the stack
 ; Parameter (R6): TOS (Top of Stack): A pointer to the current top of the stack
-; Postcondition: The subroutine has pushed (R0) onto the stack (i.e to address TOS+1). 
-;		    If the stack was already full (TOS = MAX), the subroutine has printed an
-;		    overflow error message and terminated.
-; Return Value: R6 ← updated TOS
+; Postcondition: The subroutine has pushed (R0) onto the stack (i.e to address TOS+1).
+;  If the stack was already full (TOS = MAX), the subroutine has printed an
+;  overflow error message and terminated.
+; Return Value: R6  updated TOS
 ;------------------------------------------------------------------------------------------
 .orig x3200
-				 
-ST R1, BACKUP_R1_x3200
-ST R2, BACKUP_R2_x3200
-ST R7, BACKUP_R7_x3200
+;Subroutine Instructions
 
-AND R3, R3, #0
-AND R1, R1, #0
+ST R2, BACKUP_R2
+ST R7, BACKUP_R7
 
-ADD R1, R1, R6
-NOT R1, R1
+NOT R2, R5
+ADD R2, R2, #1
 
-ADD R1, R1, #1
-ADD R1, R1, R5
+ADD R2, R6, R2
+BRzp OVERFLOW_ERROR
 
-AGENT
-  BRz PERRY
-  DOOFENSHMIRTZ
-    ADD R6, R6, #1
-    
-    STR R0, R6, #0
-    LEA R0,	newline
-    PUTS
-    
-    BR MONOGRAM
-  PERRY
-    ADD R3, R3, #1
-    
-    LEA R0, push_overflow
-    PUTS
-  MONOGRAM
-LD R1, BACKUP_R1_x3200
-LD R2, BACKUP_R2_x3200
-LD R7, BACKUP_R7_x3200
+ADD R6, R6, #1
+STR R0, R6, #0
+
+LD R2, BACKUP_R2
+LD R7, BACKUP_R7
 
 RET
-;-----------------------------------------------------------------------------------------------
-; SUB_STACK_PUSH local data
-BACKUP_R1_x3200 	.BLKW #1
-BACKUP_R2_x3200 	.BLKW #1
-BACKUP_R7_x3200 	.BLKW #1
 
-newline				.STRINGZ "\n"
+OVERFLOW_ERROR
+  ST R0, R0_ERROR
+  LEA R0, OVERFLOW_MESSAGE
+  PUTS
 
-push_overflow		.STRINGZ "\nOVERFLOWING STACK.\n"
-;===============================================================================================
+  LD R0, R0_ERROR
+  LD R2, BACKUP_R2
+  LD R7, BACKUP_R7
+
+  RET
+
+;Subroutine Data
+OVERFLOW_MESSAGE .STRINGZ "Error Overflow!!\n"
+
+R0_ERROR 		 .BLKW 1
+BACKUP_R2 		 .BLKW 1
+BACKUP_R7 		 .BLKW 1
 
 
 ;------------------------------------------------------------------------------------------
-; Subroutine: SUB_STACK_POP
-; Parameter (R4): BASE: A pointer to the base (one less than the lowest available                      
-;                       address) of the stack
+;Subroutine: SUB_STACK_POP
+; Parameter (R4): BASE: A pointer to the base (one less than the lowest available
+; address) of the stack
 ; Parameter (R5): MAX: The "highest" available address in the stack
 ; Parameter (R6): TOS (Top of Stack): A pointer to the current top of the stack
-; Postcondition: The subroutine has popped MEM[TOS] off of the stack.
-;		    If the stack was already empty (TOS = BASE), the subroutine has printed
-;                an underflow error message and terminated.
-; Return Value: R0 ← value popped off the stack
-;		   R6 ← updated TOS
+; Postcondition: The subroutine has popped MEM[top] off of the stack.
+;  If the stack was already empty (TOS = BASE), the subroutine has printed
+;  an underflow error message and terminated.
+; Return Value: R0  value popped off of the stack
+;  R6  updated TOS
 ;------------------------------------------------------------------------------------------
 .orig x3400
+;Subroutine Instructions
 
-ST R1, BACKUP_R1_x3400
-ST R2, BACKUP_R2_x3400
-ST R7, BACKUP_R7_x3400
+ST R2, BACKUP_R2_2
+ST R7, BACKUP_R7_2
 
-AND R3, R3, #0
-AND R1, R1, #0
-AND R0, R0, #0
+NOT R2, R4
+ADD R2, R2, #1
 
-ADD R1, R1, R6 
-NOT R1, R1
+ADD R2, R6, R2
+BRnz ERROR
 
-ADD R1, R1, #1
-ADD R1, R1, R4
+LDR R0, R6, #0
+ADD R6, R6, #-1
 
-BERRY
-  BRz BIRT
-  WHISTLE
-    LDR R0, R6, #0
-    LD R2, thirty
-    
-    ADD R0, R0, R2
-    OUT
-    
-    ADD R6, R6, #-1
-    LEA R0, newliner
-    PUTS
-    
-    BR GIEDROYC
-  BIRT
-    ADD R3, R3, #1
-    
-    LEA R0, pop_underflow
-    PUTS
-  GIEDROYC
- 
-LD R1, BACKUP_R1_x3400
-LD R2, BACKUP_R2_x3400
-LD R7, BACKUP_R7_x3400
+LD R2, BACKUP_R2_2
+LD R7, BACKUP_R7_2
 
 RET
 
-POPUNDERFLOW .STRINGZ "\nStack is underflowing.\n"
-;-----------------------------------------------------------------------------------------------
-; SUB_STACK_POP local data
+ERROR
+  LEA R0, UNDERFLOW_MESSAGE
+  PUTS
 
-BACKUP_R1_x3400 .BLKW #1
-BACKUP_R2_x3400 .BLKW #1
-BACKUP_R7_x3400 .BLKW #1
+  LD R2, BACKUP_R2_2
+  LD R7, BACKUP_R7_2
+  
+  RET
 
-newliner 			.STRINGZ "\n"
+;Subroutine Data
+UNDERFLOW_MESSAGE.STRINGZ "Error Underflow!!\n"
 
-pop_underflow		.STRINGZ "\UNDERFLOWING STACK.\n"
+BACKUP_R2_2		 .BLKW 1
+BACKUP_R7_2		 .BLKW 1
 
-thirty				.FILL x0030
-;===============================================================================================
+
